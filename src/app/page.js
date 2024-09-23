c2b9cbdede2d531de2faa49855c99b833209363d
jekjek160404 <apembelajaran6@gmail.com>
2024-09-23 22:00:35 +0700

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,25 @@
+import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex mt-16">
-                <div className="md:w-3/4 px-4 ">
-                    <div className="  rounded-xl">
-                    <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hi, </h1>
+      <div className="">
+        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hi, </h1>
         <h1 className=" text-6xl font-sans md:Apple Color Emoji text-blue-600/100 " > I'am Ahmad Zaki </h1>
         <h1 className=" text-6xl font-sans md:Apple Color Emoji" >Data Analyst</h1>
-      <div>
-        <br></br>
-      <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">MULAI</button>
+        <div className="flex-flex-wrap">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/profil.png"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
-                        
-                    </div>
-                </div>
-               
-                </div>
-            
+        <div>
+          <button className=" mt-20 rounded-lg bg-blue-500 w-48 min-h-12 text-white" >Get Started</button>
+        </div>
+      </div>
     </>
   );
 }
-
